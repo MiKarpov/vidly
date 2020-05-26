0e55c543c9f4ed7e48f8d7885d8eb7153b7f98ce
Mikhail Karpov <mikhailkarpov@hotmail.com>
2020-05-27 01:42:32 +0200

    Implementing movies list

---
+++ src/components/movies.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  render() {
+    const totalMovies = this.state.movies.length;
+
+    if (totalMovies === 0) return <p>There are no movies in database</p>;
+
+    return (
+      <React.Fragment>
+        <p>Showing {totalMovies} movies</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie._id)}
+                    className="btn btn-danger btn-sm">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+
+  handleDelete = (movieId) => {
+    const newMovies = this.state.movies.filter((m) => m._id !== movieId);
+    this.setState({ movies: newMovies });
+  };
+}
+
+export default Movies;
