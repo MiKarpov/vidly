5e757eb4455215fad7b534ee436cc5a2e11189c1
MiKarpov <mikhailkarpov@hotmail.com>
2020-05-28 18:46:20 +0300

    Implemented navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
-import Movies from "./components/movies";
+import Movies from './components/movies';
+import Customers from './components/customers';
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
+import NavBar from './components/common/navBar';
+import MovieForm from './components/movieForm';
+
+import './App.css';
 
 function App() {
   return (
-    <main className="container">
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className='container'>
+        <Switch>
+          <Route path='/movies/:id' component={MovieForm} />
+          <Route path='/movies' component={Movies} />
+          <Route path='/customers' component={Customers} />
+          <Route path='/rentals' component={Rentals} />
+          <Route path='/not-found' component={NotFound} />
+          <Redirect from='/' exact to='/movies' />
+          <Redirect to='/not-found' />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import Like from "./common/like";
+import React from 'react';
+import Like from './common/like';
+import { Link } from 'react-router-dom';
 
 const MoviesTable = (props) => {
   const { movies, onDelete, onLike, onSort } = props;
 
   return (
-    <table className="table">
+    <table className='table'>
       <thead>
         <tr>
-          <th onClick={() => onSort("title")}>
-            <span className="clickable">Title</span>
+          <th onClick={() => onSort('title')}>
+            <span className='clickable'>Title</span>
           </th>
-          <th onClick={() => onSort("genre.name")}>
-            <span className="clickable">Genre</span>
+          <th onClick={() => onSort('genre.name')}>
+            <span className='clickable'>Genre</span>
           </th>
-          <th onClick={() => onSort("numbersInStock")}>
-            <span className="clickable">Stock</span>
+          <th onClick={() => onSort('numbersInStock')}>
+            <span className='clickable'>Stock</span>
           </th>
-          <th onClick={() => onSort("dailyRentalRate")}>
-            <span className="clickable">Rate</span>
+          <th onClick={() => onSort('dailyRentalRate')}>
+            <span className='clickable'>Rate</span>
           </th>
           <th />
           <th />
@@ -27,7 +28,9 @@ const MoviesTable = (props) => {
       <tbody>
         {movies.map((movie) => (
           <tr key={movie._id}>
-            <td>{movie.title}</td>
+            <td>
+              <Link to={`/movies/${movie._id}`}>{movie.title}</Link>
+            </td>
             <td>{movie.genre.name}</td>
             <td>{movie.numberInStock}</td>
             <td>{movie.dailyRentalRate}</td>
@@ -35,7 +38,7 @@ const MoviesTable = (props) => {
               <Like liked={movie.liked} onClick={() => onLike(movie)} />
             </td>
             <td>
-              <button onClick={() => onDelete(movie._id)} className="btn btn-danger btn-sm">
+              <button onClick={() => onDelete(movie._id)} className='btn btn-danger btn-sm'>
                 Delete
               </button>
             </td>
