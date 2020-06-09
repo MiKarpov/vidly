ae2f2a1c1313222bf3ca5d3a39a7851f25d7d909
MiKarpov <mikhailkarpov@hotmail.com>
2020-06-09 16:16:45 +0300

    fixed endpoints

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,8 @@
-import httpService from "./httpService";
-import logger from "./logService";
-import { apiUrl } from "../config.json";
+import httpService from './httpService';
+import logger from './logService';
+import { apiUrl } from '../config.json';
+
+const apiEndpoint = apiUrl + '/account';
 
 export async function register(email, password, matchingPassword) {
   const body = {
@@ -9,8 +11,8 @@ export async function register(email, password, matchingPassword) {
     matchingPassword: matchingPassword,
   };
 
-  logger.log("Registering user " + JSON.stringify(body));
-  const { data } = await httpService.post(apiUrl + "/registrations", body);
+  logger.log('Registering user ' + JSON.stringify(body));
+  const { data } = await httpService.post(apiEndpoint + '/register', body);
   saveJwtAndUser(data);
 }
 
@@ -20,27 +22,27 @@ export async function login(email, password) {
     password: password,
   };
 
-  logger.log("Authentication user " + JSON.stringify(body));
-  const { data } = await httpService.post(apiUrl + "/auth", body);
+  logger.log('Authenticating user ' + JSON.stringify(body));
+  const { data } = await httpService.post(apiEndpoint + '/auth', body);
   saveJwtAndUser(data);
 }
 
 export function logout() {
-  localStorage.removeItem("jwt");
-  localStorage.removeItem("user");
+  localStorage.removeItem('jwt');
+  localStorage.removeItem('user');
 }
 
 export function getJwt() {
-  return localStorage.getItem("token");
+  return localStorage.getItem('token');
 }
 
 export function getUser() {
-  return localStorage.getItem("user");
+  return localStorage.getItem('user');
 }
 
 function saveJwtAndUser(data) {
-  localStorage.setItem("token", data.jwt);
-  localStorage.setItem("user", data.email);
+  localStorage.setItem('token', data.jwt);
+  localStorage.setItem('user', data.email);
 }
 
 export default {
