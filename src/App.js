edc87d8077c2ed600ded5a175de60d30ca02ae5d
MiKarpov <mikhailkarpov@hotmail.com>
2020-06-09 17:37:28 +0300

    Refactoring navigation bar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
-import React, { Component } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-import Movies from "./components/movies";
-import Customers from "./components/customers";
-import NotFound from "./components/notFound";
-import Rentals from "./components/rentals";
-import NavBar from "./components/common/navBar";
-import MovieForm from "./components/movieForm";
-import LoginForm from "./components/login";
-import RegisterForm from "./components/registerForm";
+import Movies from './components/movies';
+import Customers from './components/customers';
+import NotFound from './components/notFound';
+import Rentals from './components/rentals';
+import NavBar from './components/common/navBar';
+import MovieForm from './components/movieForm';
+import LoginForm from './components/login';
+import Logout from './components/logout';
+import RegisterForm from './components/registerForm';
 
-import authService from "./services/authService";
+import authService from './services/authService';
 
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 class App extends Component {
-  state = {};
+  state = { currentUser: '' };
 
   componentDidMount() {
-    const user = authService.getUser();
-    console.log("Current user", user);
+    const currentUser = authService.getUser();
+    if (currentUser) {
+      this.setState({ currentUser });
+    }
   }
 
   render() {
     return (
       <React.Fragment>
         <ToastContainer />
-        <NavBar />
+        <NavBar user={this.state.currentUser} />
         <main className='container'>
           <Switch>
             <Route path='/login' component={LoginForm} />
+            <Route path='/logout' component={Logout} />
             <Route path='/register' component={RegisterForm} />
             <Route path='/movies/:id' component={MovieForm} />
             <Route path='/movies' component={Movies} />
--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -1,51 +1,58 @@
-import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
+  let userDashboard = (
+    <React.Fragment>
+      <li className='nav-item'>
+        <NavLink className='nav-link' to='/login'>
+          Login
+        </NavLink>
+      </li>
+      <li className='nav-item'>
+        <NavLink className='nav-link' to='/register'>
+          Register
+        </NavLink>
+      </li>
+    </React.Fragment>
+  );
+
+  if (user) {
+    userDashboard = (
+      <React.Fragment>
+        <li className='nav-item'>
+          <Navbar.Text>{user}</Navbar.Text>
+        </li>
+        <li className='nav-item'>
+          <NavLink className='nav-link' to='/logout'>
+            Logout
+          </NavLink>
+        </li>
+      </React.Fragment>
+    );
+  }
+
   return (
-    <div className="bg-light">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light container">
-        <Link className="navbar-brand" to="/">
+    <div className='bg-light'>
+      <nav className='navbar navbar-expand-lg navbar-light bg-light container'>
+        <Link className='navbar-brand' to='/'>
           Vidly
         </Link>
+
         <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarNav'
+          aria-controls='navbarNav'
+          aria-expanded='false'
+          aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon'></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/movies">
-                Movies
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/customers">
-                Customers
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/rentals">
-                Rentals
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/login">
-                Login
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/register">
-                Register
-              </NavLink>
-            </li>
-          </ul>
+
+        <div className='collapse navbar-collapse justify-content-end' id='navbarNav'>
+          <ul className='navbar-nav'>{userDashboard}</ul>
         </div>
       </nav>
     </div>
